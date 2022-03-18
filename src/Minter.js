9a650965cada5f172e0f8281996bbbaec8f303c0
topcook <codecooker1996@gmail.com>
2022-03-18 17:24:43 -0400

    This is the initial version of frontend.

---
+++ src/Minter.js
@@ -0,0 +1,105 @@
+import { useEffect, useState } from "react";
+import { connectWallet, getCurrentWalletConnected, mintNFT } from "./utils/interact";
+
+const Minter = (props) => {
+
+  //State variables
+  const [walletAddress, setWallet] = useState("");
+  const [status, setStatus] = useState("");
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [url, setURL] = useState("");
+
+  useEffect(async () => { //TODO: implement
+    const { address, status } = await getCurrentWalletConnected();
+    setWallet(address)
+    setStatus(status);
+
+    addWalletListener();
+  }, []);
+
+  const connectWalletPressed = async () => { //TODO: implement
+    const walletStatus = await connectWallet();
+    setWallet(walletStatus.address)
+    setStatus(walletStatus.status);
+  };
+
+  function addWalletListener() {
+    if (window.ethereum) {
+      window.ethereum.on("accountsChanged", (accounts) => {
+        if (accounts.length > 0) {
+          setWallet(accounts[0]);
+          setStatus("👆🏽 Write a message in the text-field above.");
+        } else {
+          setWallet("");
+          setStatus("🦊 Connect to Metamask using the top right button.");
+        }
+      });
+    } else {
+      setStatus(
+        <p>
+          {" "}
+          🦊{" "}
+          <a target="_blank" href={`https://metamask.io/download.html`}>
+            You must install Metamask, a virtual Ethereum wallet, in your
+            browser.
+          </a>
+        </p>
+      );
+    }
+  }
+
+  const onMintPressed = async () => { //TODO: implement
+    const { status } = await mintNFT(url, name, description);
+    setStatus(status);
+  };
+
+  return (
+    <div className="Minter">
+      <button id="walletButton" onClick={connectWalletPressed}>
+        {walletAddress.length > 0 ? (
+          "Connected: " +
+          String(walletAddress).substring(0, 6) +
+          "..." +
+          String(walletAddress).substring(38)
+        ) : (
+          <span>Connect Wallet</span>
+        )}
+      </button>
+
+      <br></br>
+      <h1 id="title">🧙‍♂️ Alchemy NFT Minter</h1>
+      <p>
+        Simply add your asset's link, name, and description, then press "Mint."
+      </p>
+      <form>
+        <h2>🖼 Link to asset: </h2>
+        <input
+          type="text"
+          placeholder="e.g. https://gateway.pinata.cloud/ipfs/<hash>"
+          onChange={(event) => setURL(event.target.value)}
+        />
+        <h2>🤔 Name: </h2>
+        <input
+          type="text"
+          placeholder="e.g. My first NFT!"
+          onChange={(event) => setName(event.target.value)}
+        />
+        <h2>✍️ Description: </h2>
+        <input
+          type="text"
+          placeholder="e.g. Even cooler than cryptokitties ;)"
+          onChange={(event) => setDescription(event.target.value)}
+        />
+      </form>
+      <button id="mintButton" onClick={onMintPressed}>
+        Mint NFT
+      </button>
+      <p id="status">
+        {status}
+      </p>
+    </div>
+  );
+};
+
+export default Minter;
---
+++ src/utils/interact.js
@@ -0,0 +1,149 @@
+import { pinJSONToIPFS } from './pinata';
+
+require('dotenv').config();
+const alchemyKey = process.env.REACT_APP_ALCHEMY_KEY;
+const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+const web3 = createAlchemyWeb3(alchemyKey);
+
+const contractABI = require('../contract-abi.json')
+const contractAddress = "0xD6CDB48a4c5e4176701b16c4998614EF9343eEC9";
+
+export const connectWallet = async () => {
+    if (window.ethereum) {
+        try {
+            const addressArray = await window.ethereum.request({
+                method: "eth_requestAccounts"
+            });
+            const obj = {
+                status: "👆🏽 Write a message in the text-field above.",
+                address: addressArray[0],
+            };
+
+            return obj;
+        } catch (err) {
+            return {
+                address: "",
+                status: "😥 " + err.message,
+            };
+        }
+    } else {
+        return {
+            address: "",
+            status: (
+                <span>
+                    <p>
+                        {" "}
+                        🦊{" "}
+                        <a target="_blank" href={`https://metamask.io/download.html`}>
+                            You must install Metamask, a virtual Ethereum wallet, in your
+                            browser.
+                        </a>
+                    </p>
+                </span>
+            ),
+        };
+    }
+}
+
+export const getCurrentWalletConnected = async () => {
+    if (window.ethereum) {
+        try {
+            const addressArray = await window.ethereum.request({
+                method: "eth_accounts",
+            });
+            if (addressArray.length > 0) {
+                console.log("123123123123123123", addressArray[0])
+                const nfts = await web3.alchemy.getNfts({owner: contractAddress})
+                console.log("====================userOwnedNfts==================", nfts)
+                return {
+                    address: addressArray[0],
+                    status: "👆🏽 Write a message in the text-field above.",
+                };
+            } else {
+                return {
+                    address: "",
+                    status: "🦊 Connect to Metamask using the top right button.",
+                };
+            }
+        } catch (err) {
+            return {
+                address: "",
+                status: "😥 " + err.message,
+            };
+        }
+    } else {
+        return {
+            address: "",
+            status: (
+                <span>
+                    <p>
+                        {" "}
+                        🦊{" "}
+                        <a target="_blank" href={`https://metamask.io/download.html`}>
+                            You must install Metamask, a virtual Ethereum wallet, in your
+                            browser.
+                        </a>
+                    </p>
+                </span>
+            ),
+        };
+    }
+};
+
+export const mintNFT = async (url, name, description) => {
+
+    //error handling
+    if (url.trim() == "" || (name.trim() == "" || description.trim() == "")) {
+        return {
+            success: false,
+            status: "❗Please make sure all fields are completed before minting.",
+        }
+    }
+
+    //make metadata
+    const metadata = new Object();
+    metadata.name = name;
+    metadata.image = url;
+    metadata.description = description;
+
+    console.log("=========================metadata========================", metadata)
+
+    //pinata pin request
+    const pinataResponse = await pinJSONToIPFS(metadata);
+    if (!pinataResponse.success) {
+        return {
+            success: false,
+            status: "😢 Something went wrong while uploading your tokenURI.",
+        }
+    }
+    const tokenURI = pinataResponse.pinataUrl;
+    console.log("=========================tokenURI=======================", tokenURI)
+
+    //load smart contract
+    window.contract = await new web3.eth.Contract(contractABI, contractAddress);//loadContract();
+
+    //set up your Ethereum transaction
+    const transactionParameters = {
+        to: contractAddress, // Required except during contract publications.
+        from: window.ethereum.selectedAddress, // must match user's active address.
+        'data': window.contract.methods.mintNFT(window.ethereum.selectedAddress, tokenURI).encodeABI() //make call to NFT smart contract 
+    };
+
+    //sign transaction via Metamask
+    try {
+        const txHash = await window.ethereum
+            .request({
+                method: 'eth_sendTransaction',
+                params: [transactionParameters],
+            });
+        return {
+            success: true,
+            status: "✅ Check out your transaction on Etherscan: https://ropsten.etherscan.io/tx/" + txHash
+        }
+    } catch (error) {
+        return {
+            success: false,
+            status: "😥 Something went wrong: " + error.message
+        }
+    }
+}
